704c94a51ea1447e3dfbebbc2aae882613594d26
pozafly <pozafly@gmail.com>
2025-01-18 16:35:45 +0900

    feat: jpa 사용해서 DB에 값 넣기

--- src/main/java/hellojpa/JpaMain.java
+++ src/main/java/hellojpa/JpaMain.java
@@ -9,8 +9,22 @@ public class JpaMain {
         EntityManagerFactory emf = Persistence.createEntityManagerFactory("hello");
         EntityManager em = emf.createEntityManager();
         //code
+        EntityTransaction tx = em.getTransaction();
+        tx.begin();
 
-        em.close();
+        try {
+            Member member = new Member();
+            member.setId(2L);
+            member.setName("helloB");
+            em.persist(member);
+
+            tx.commit();
+        } catch (Exception e) {
+            tx.rollback();
+        } finally {
+            em.close();
+        }
+
         emf.close();
     }
 }

1b8002992687dd28d9d042b28e10a4d368daf0c4
pozafly <pozafly@gmail.com>
2025-01-23 22:05:37 +0900

    객체 연관관계 사용

--- src/main/java/jpabook/hellojpa/JpaMain.java
+++ src/main/java/jpabook/hellojpa/JpaMain.java
@@ -1,4 +1,4 @@
-package jpabook;
+package jpabook.hellojpa;
 
 import jakarta.persistence.EntityManager;
 import jakarta.persistence.EntityManagerFactory;
@@ -16,6 +16,27 @@ public class JpaMain {
         tx.begin();
 
         try {
+
+            // 저장
+            Team team = new Team();
+            team.setName("TeamA");
+            em.persist(team);
+
+            Member member = new Member();
+            member.setName("member1");
+            member.setTeam(team);
+            em.persist(member);
+
+            em.flush();
+            em.clear();
+
+            Member findMember = em.find(Member.class, member.getId());
+
+            // Long teamId = findMember.getTeamId();
+            // em.find(Team.class, teamId);
+
+            Team findTeam = findMember.getTeam();
+            System.out.println("findTeam = " + findTeam.getName());
 
             tx.commit();
         } catch (Exception e) {

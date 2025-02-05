da163534a021427c9024c306fa01c116af975d02
pozafly <pozafly@gmail.com>
2025-02-05 19:55:13 +0900

    양방향 연관관계 매핑

--- src/main/java/jpabook/hellojpa/JpaMain.java
+++ src/main/java/jpabook/hellojpa/JpaMain.java
@@ -1,52 +1,52 @@
-// package jpabook.hellojpa;
-//
-// import jakarta.persistence.EntityManager;
-// import jakarta.persistence.EntityManagerFactory;
-// import jakarta.persistence.EntityTransaction;
-// import jakarta.persistence.Persistence;
-//
-// import java.util.List;
-//
-// public class JpaMain {
-//
-//     public static void main(String[] args) {
-//
-//         EntityManagerFactory emf = Persistence.createEntityManagerFactory("hello");
-//         EntityManager em = emf.createEntityManager();
-//         // code
-//         EntityTransaction tx = em.getTransaction();
-//         tx.begin();
-//
-//         try {
-//             // 저장
-//             Team team = new Team();
-//             team.setName("TeamA");
-//             em.persist(team);
-//
-//             Member member = new Member();
-//             member.setName("member1");
-//             member.changeTeam(team);
-//             em.persist(member);
-//
-//             team.getMembers().add(member);
-//
-//             em.flush();
-//             em.clear();
-//
-//             Team findTeam = em.find(Team.class, team.getId());
-//             List<Member> members = findTeam.getMembers();
-//
-//             for (Member m : members) {
-//                 System.out.println("m.getName() = " + m.getName());
-//             }
-//
-//             tx.commit();
-//         } catch (Exception e) {
-//             tx.rollback();
-//         } finally {
-//             em.close();
-//         }
-//
-//         emf.close();
-//     }
-// }
+package jpabook.hellojpa;
+
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.EntityManagerFactory;
+import jakarta.persistence.EntityTransaction;
+import jakarta.persistence.Persistence;
+
+import java.util.List;
+
+public class JpaMain {
+
+    public static void main(String[] args) {
+
+        EntityManagerFactory emf = Persistence.createEntityManagerFactory("hello");
+        EntityManager em = emf.createEntityManager();
+        // code
+        EntityTransaction tx = em.getTransaction();
+        tx.begin();
+
+        try {
+            // 저장
+            // Team team = new Team();
+            // team.setName("TeamA");
+            // em.persist(team);
+            //
+            // Member member = new Member();
+            // member.setName("member1");
+            // member.changeTeam(team);
+            // em.persist(member);
+            //
+            // team.getMembers().add(member);
+            //
+            // em.flush();
+            // em.clear();
+            //
+            // Team findTeam = em.find(Team.class, team.getId());
+            // List<Member> members = findTeam.getMembers();
+            //
+            // for (Member m : members) {
+            //     System.out.println("m.getName() = " + m.getName());
+            // }
+
+            tx.commit();
+        } catch (Exception e) {
+            tx.rollback();
+        } finally {
+            em.close();
+        }
+
+        emf.close();
+    }
+}
